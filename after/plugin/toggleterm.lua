0b08023acab6507d658e821a6d1026efe832c029
anthonymolinari <amolinari017@gmail.com>
2025-06-14 12:51:55 -0700

    setup toggle terminal

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,18 @@
+local tg = require('toggleterm')
+local Terminal = require('toggleterm.terminal').Terminal
+
+tg.setup{
+    size = 10,
+    open_mapping = [[<C-j>]],
+    authchdir = false,
+    auto_scroll = true,
+    direction = 'horizontal',
+}
+
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true })
