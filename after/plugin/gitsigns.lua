e238861380991216de296a4fa5caf16beb269dc1
anthonymolinari <amolinari017@gmail.com>
2025-07-02 10:44:17 -0700

    fix keybinding overlap

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -49,7 +49,7 @@ require('gitsigns').setup({
         end, {expr=true})
 
         -- actions
-        map('n', '<leader>hs', gs.stage_hunk)
+        map('n', '<leader>sh', gs.stage_hunk)
         map('n', '<leader>hr', gs.reset_hunk)
         map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
         map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
